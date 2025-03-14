ae17a9a980ebfa24df54d06f5017de959d28d205
andresavasconcelos <andresa.vasconcelos-30@hotmail.com>
2025-03-14 01:05:33 -0300

    main - first commit

---
+++ src/main/java/com/project/quickschedule/QuickscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.project.quickschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quickschedule/QuickscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quickschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
